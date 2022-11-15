5f3a0adbe5e232abbfb082b14066d5819eefaecc
Ernesto Rodriguez <erodriiguezz@icloud.com>
2022-11-15 11:48:38 -0500

    add list questions routes

--- controllers/list.controllers.js
+++ controllers/list.controllers.js
@@ -22,6 +22,23 @@ export const listAttendees = async (req, res) => {
 
 //
 //
+export const listQuestions = async (req, res) => {
+  try {
+    const requestParams = new URLSearchParams({
+      accesstoken: await getToken(),
+      eventid: getParamsFromUrl(req.url).eventid,
+    });
+
+    const requestResponse = await needle("get", `https://api-na.eventscloud.com/api/v2/ereg/listQuestions.json?${requestParams}`);
+
+    res.status(200).json(requestResponse.body);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+//
+//
 export const listSessions = async (req, res) => {
   try {
     const requestParams = new URLSearchParams({
--- routes/list.routes.js
+++ routes/list.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { listAttendees, listSessions, listSpeakers } from "../controllers/list.controllers.js";
+import { listAttendees, listQuestions, listSessions, listSpeakers } from "../controllers/list.controllers.js";
 
 const router = express.Router();
 
 router.get("/listAttendees", listAttendees);
+router.get("/listQuestions", listQuestions);
 router.get("/listSessions", listSessions);
 router.get("/listSpeakers", listSpeakers);
 
